44cc757021352469cc4b0d83febd41ee971af3d9
Pranay Vasanth <148627387+pranayvasanth@users.noreply.github.com>
2024-12-15 06:01:36 +0000

    smaple

---
+++ blogs/simple.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class simple {
+    
+    public static void main( String args[])
+    {
+        @SuppressWarnings("resource")
+        Scanner sc=new Scanner(System.in);
+
+        System.out.println("Enter your name");
+        String n=sc.nextLine();
+        System.out.println("Enter your age");
+        int a=sc.nextInt();
+        if(a>=18)
+        {
+            System.out.println("Your an Adult");
+            System.out.println("Name:"+n);
+            System.out.println("Age:"+a);
+        }
+    }
+}
