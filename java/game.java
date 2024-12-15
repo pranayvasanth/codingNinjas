2b946c85b0b5a1ef72e73989e6e78aa1b3085a5a
Pranay Vasanth <148627387+pranayvasanth@users.noreply.github.com>
2024-12-15 12:35:15 +0000

    Rock Paper Scissors

--- java/game.java
+++ java/game.java
@@ -7,11 +7,16 @@ public class game{
 
         System.out.println("Enter the your choice \t 1.Rock \t 2.Paper \t 3.Scissors");
         int uc=s.nextInt();
+        
 
-
+        switch (uc) {
+          case 1 -> System.out.println("Your choice : Rock");
+          case 2 -> System.out.println("Your choice : Paper");
+          case 3 -> System.out.println("Your choice : Scissors");
+      }
         Random ran =new Random();
 
-        int ran_n = ran.nextInt(3);
+        int ran_n = ran.nextInt(3)+1;
 
       switch (ran_n) {
           case 1 -> System.out.println("computer choise: Rock \t" );
@@ -21,13 +26,15 @@ public class game{
       }
       if(uc==ran_n)
       {
-        System.out.println("Draw try again");
+        System.out.println("Draw Try Again!");
       }
-      else if(uc>ran_n){
+      else if(uc==1 && ran_n==3 || uc==2 && ran_n==1 || uc==3 && ran_n==2)
+      {
         System.out.println("You Win :)");
       }
       else{
-        System.out.println("You Lose :(");
+        System.out.println("You Lose :( \n Better Luck next Time! ;-)");
       }
-    }
+    
+  }
 }
